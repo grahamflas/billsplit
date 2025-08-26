1540d34dde0595a84afe1aef51942e67656d900c
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-26 07:05:52 +0200

    Replace deprecated status code in GroupsController
    Specs showed a warning that `:unprocessable_entity` is deprecated in favor of
    `:unprocessable_content`.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,7 @@ class GroupsController < ApplicationController
 
       redirect_to @group
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_content
     end
   end
 
