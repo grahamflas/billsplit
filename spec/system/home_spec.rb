c868ccee6fbb65ed7b56c5cba48806e94dad2011
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-26 07:47:12 +0200

    Show groups on Home page (#13)
    When a logged-in user is on the Home page, show cards for `Group`s belonging to
    `current_user`. Also allow users to create a new `Group` from the Home page.

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe "Home Page", type: :system, js: true do
+  scenario "shows the logged-in user a list of their groups with links to add new groups and view ones" do
+    group_1 = create(:group)
+    group_2 = create(:group)
+    group_3 = create(:group)
+    other_group = create(:group)
+
+    user = create(
+      :user,
+      groups: [
+        group_1,
+        group_2,
+        group_3,
+      ]
+    )
+
+    _other_user = create(
+      :user,
+      groups: [ other_group ],
+    )
+
+    sign_in user
+
+    visit root_path
+
+    expect(page).to have_content(group_1.name)
+    expect(page).to have_content(group_2.name)
+    expect(page).to have_content(group_3.name)
+    expect(page).not_to have_content(other_group.name)
+
+    within "#group-#{group_1.id}" do
+      expect(page).to have_link("View group", href: group_path(group_1))
+    end
+
+    add_new_group_button_text = "+ Add Group"
+    expect(page).to have_link(add_new_group_button_text, href: new_group_path)
+
+    click_link add_new_group_button_text
+
+    fill_in "Group name", with: "My New Group"
+
+    click_button "Create Group"
+
+    expect(page).to have_content("My New Group created")
+  end
+end
