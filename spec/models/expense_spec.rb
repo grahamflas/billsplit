b583d447dbd034e45c03fc1fc6e00bd0b64130fb
Graham Flaspoehler <grahamflas@gmail.com>
2025-08-26 07:46:20 +0200

    Add missing model specs to silence RSpec warnings

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:group) }
+  end
 end
