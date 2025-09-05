baa5e7b5ff544e9d058c678d27b8296f61fe6d32
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-05 07:50:26 +0200

    Include expenses in query for groups on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @groups = current_user.
       groups.
       includes(:users).
+      includes(:expenses).
       to_api.
       serializable_hash
   end
