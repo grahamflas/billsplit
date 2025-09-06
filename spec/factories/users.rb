164d06cd8ee4048da7a0e56d69394e2617563080
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-06 09:04:49 +0200

    Update users factory to create first_name, last_name
    Now that we've added non-nullable `users.first_name` and `users.last_name`
    columns, we need to update the factory to also create these when creating a
    user.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "testuser#{n}@email.com" }
     password { "password" }
+    sequence(:first_name) { |n| "User#{n} First Name" }
+    sequence(:last_name) { |n| "User#{n} Last Name" }
   end
 end
