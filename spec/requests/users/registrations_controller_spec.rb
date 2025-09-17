699b2720cca2faa7ef0c60cbb10ec2bd39c362ba
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-17 11:06:53 +0200

    Permit new user params when signing up (#44)

---
+++ spec/requests/users/registrations_controller_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Users::RegistrationsController, type: :request do
+  describe "POST /users" do
+    context "with valid params" do
+      it "creates the user and redirects to the home page" do
+        password = "password"
+
+        expect do
+          post user_registration_path, params: {
+            user: {
+              first_name: "Liz",
+              last_name: "Lemon",
+              email: "liz@tgs.com",
+              password:,
+              password_confirmation: password,
+            }
+          }
+        end.to change(User, :count).by(1)
+
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+end
