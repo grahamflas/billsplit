b6e32ae803cbfc6061e990c766d16cd9a11ff6f6
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-17 16:12:43 +0200

    Add styles to React Group form (#46)

--- app/javascript/apps/GroupForm.tsx
+++ app/javascript/apps/GroupForm.tsx
@@ -30,36 +30,50 @@ const GroupForm = ({ addableUsers, group }: Props) => {
   };
 
   return (
-    <div>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => console.log(values)}
-      >
-        <Form>
-          <label htmlFor="name">Group Name</label>
-          <Field type="text" name="name" id="name" />
-
-          {userOptions() && (
-            <>
-              <label htmlFor="userIds">Group Members</label>
+    <div className="flex items-center justify-center py-20">
+      <div className="flex flex-col bg-white shadow rounded-lg p-8 max-w-md">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values) => console.log(values)}
+        >
+          <Form>
+            <div className="mb-6">
               <Field
-                name="userIds"
-                id="userIds"
-                component={GroupFormUserSelect}
-                options={userOptions()}
-                isMulti
+                type="text"
+                name="name"
+                id="name"
+                className="border border-indigo-50 rounded-md bg-indigo-50 focus:outline-indigo-400 px-3 py-2 w-full"
               />
-            </>
-          )}
+              <label htmlFor="name" className="text-gray-500">
+                Group Name
+              </label>
+            </div>
 
-          <button
-            type="submit"
-            className="w-full bg-indigo-500 hover:bg-indigo-600 focus:outline-indigo-900 rounded-md text-white py-2 mb-6 cursor:pointer"
-          >
-            Create Group
-          </button>
-        </Form>
-      </Formik>
+            {userOptions() && (
+              <div className="mb-6">
+                <label htmlFor="userIds" className="text-gray-500">
+                  Group Members
+                </label>
+                <Field
+                  name="userIds"
+                  id="userIds"
+                  component={GroupFormUserSelect}
+                  options={userOptions()}
+                  isMulti
+                  className="border border-indigo-50 rounded-md bg-indigo-50 focus:outline-indigo-400 px-3 py-2 w-full"
+                />
+              </div>
+            )}
+
+            <button
+              type="submit"
+              className="w-full bg-indigo-500 hover:bg-indigo-600 focus:outline-indigo-900 rounded-md text-white py-2 mb-6 cursor:pointer"
+            >
+              Create Group
+            </button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
