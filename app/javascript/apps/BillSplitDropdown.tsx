090ee293d8eddc57c7c4ddd19fd9d29840294ee8
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-19 17:17:31 +0200

    Add shadow to dropdown used for user menu

--- app/javascript/apps/BillSplitDropdown.tsx
+++ app/javascript/apps/BillSplitDropdown.tsx
@@ -17,7 +17,7 @@ const BillSplitDropdown = ({ buttonContent, menuItems, testId }: Props) => {
         transition
         anchor="bottom end"
         data-test={`${testId}-dropdown`}
-        className="w-40 origin-top-right rounded-xl bg-white p-1 transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
+        className="shadow-2xl w-40 origin-top-right rounded-xl bg-white p-1 transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
       >
         {menuItems.map((menuItem) => {
           return <MenuItem>{menuItem}</MenuItem>;
