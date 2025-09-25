b898c439b8afbbaf2bfb68b3ed4794eb1b5ddf58
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-25 08:44:08 +0200

    Add demo column to users and groups (#68)
    When a user signs up, we'll create some demo data. We want to be able to
    distinguish real from demo data, so we add a demo column to groups and users

---
+++ db/migrate/20250925064216_add_demo_to_users.rb
@@ -0,0 +1,5 @@
+class AddDemoToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :demo, :boolean, null: false, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_19_195610) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_25_064309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_19_195610) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "demo", default: false, null: false
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -88,6 +89,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_19_195610) do
     t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.boolean "demo", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
