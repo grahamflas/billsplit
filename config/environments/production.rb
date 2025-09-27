5a9b5b4269ed1ca3298d2f0f6fe528df391974ec
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-27 19:37:54 +0200

    Update production smtp_settings
    I'm currently getting a Net::OpenTimeout error when trying to send email from
    production. In this commit, we update the production.rb smtp settings to try to
    resolve this.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
     address:         "smtp.gmail.com",
     port:            587,
-    domain:          "google.com",
+    domain:          "gmail.com",
     user_name:       ENV["EMAIL_USERNAME"],
     password:        ENV["EMAIL_APP_PASSWORD"],
     authentication:  "plain",
