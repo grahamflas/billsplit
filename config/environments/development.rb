87ca3031b5186c8b24a079cfd71ce724d485e342
Graham Flaspoehler <grahamflas@gmail.com>
2025-09-28 10:38:20 +0200

    Send email with SendGrid
    Working in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,18 +31,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:         "smtp.gmail.com",
-    port:            587,
-    domain:          "gmail.com",
-    user_name:       ENV["EMAIL_USERNAME"],
-    password:        ENV["EMAIL_APP_PASSWORD"],
-    authentication:  "plain",
-    enable_starttls_auto: true,
-    open_timeout:    5,
-    read_timeout:    5
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:         "smtp.gmail.com",
+  #   port:            587,
+  #   domain:          "gmail.com",
+  #   user_name:       ENV["EMAIL_USERNAME"],
+  #   password:        ENV["EMAIL_APP_PASSWORD"],
+  #   authentication:  "plain",
+  #   enable_starttls_auto: true,
+  #   open_timeout:    5,
+  #   read_timeout:    5
+  # }
 
 
   # Don't care if the mailer can't send.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,18 +68,18 @@ Rails.application.configure do
     #   port: 587,
     #   authentication: :plain
     # }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-    address:         "smtp.gmail.com",
-    port:            587,
-    domain:          "gmail.com",
-    user_name:       ENV["EMAIL_USERNAME"],
-    password:        ENV["EMAIL_APP_PASSWORD"],
-    authentication:  "plain",
-    enable_starttls_auto: true,
-    open_timeout:    5,
-    read_timeout:    5
-  }
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #   address:         "smtp.gmail.com",
+  #   port:            587,
+  #   domain:          "gmail.com",
+  #   user_name:       ENV["EMAIL_USERNAME"],
+  #   password:        ENV["EMAIL_APP_PASSWORD"],
+  #   authentication:  "plain",
+  #   enable_starttls_auto: true,
+  #   open_timeout:    5,
+  #   read_timeout:    5
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
